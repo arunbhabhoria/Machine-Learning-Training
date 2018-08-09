18ce51fde99a1a19074f6d8fd904b44211056831
Arun Bhabhoria <35632765+arunbhabhoria@users.noreply.github.com>
2018-08-09 17:06:08 +0530

    Update assigment1.py

--- assigment1.py
+++ assigment1.py
@@ -1,6 +1,20 @@
-#Q1. Slice the given list:
+Q1. Slice the given list:
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-         #1. by using slicing from the front (positive)
-print(x[7:12])
-         #2. by using slicing from the back (negative)
-print(x[-9:-4])
+      
+print(x[7:12]) #slicing from the front 
+print(x[-9:-4]) #slicing from the Back
+ output:-
+[8, 9, 10, 11, 12]
+[8, 9, 10, 11, 12]
+
+Q2. Find EVEN NUMBERS using slicing in above list
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+print(x[1::2])
+
+output:- [2, 4, 6, 8, 10, 12, 14, 16]
+
+Q3. Print every fourth number using slicing in above list.
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+print(x[0::4])
+Output:-
+[1, 5, 9, 13]
