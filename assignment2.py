5b2bd69b0ba44ed9cd9ed2b96ec35907b8fe7f77
Arun Bhabhoria <35632765+arunbhabhoria@users.noreply.github.com>
2018-08-09 17:30:22 +0530

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,20 @@
+# Machine-Learning-Training
+
+Q.1 - Create a numpy array with 10 elements of the shape(10,1) using np.random and find out the mean of the elements using basic numpy functions.
+sol:- 
+import numpy as np
+from array import *
+a=np.random.rand(10,1) # random array of shape(10,1)
+m=np.mean(a)
+print("Mean:",m)
+
+Q.2 - Create a numpy array with 20 elements of the shape(20,1) using np.random find the variance and standard deviation of the elements.
+SOl:-
+import numpy as np
+from array import *
+a=np.random.rand(20,1) # random array of shape(20,1)
+v=np.var(a)
+s=np.std(a)
+print("Variance:", v)
+print("Standard deviation:",s)
+
